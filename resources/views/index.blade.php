3b1eeca982da0311589639e475a93744fbcafa1b
gthanksg <alamovazamjon@gmail.com>
2023-04-30 18:32:58 +0300

    initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.default')
+
+@section('title', 'main')
+
+@section('content')
+
+    <div class="uk-container uk-margin-large-top">
+        <h3>Welcome!</h3>
+    </div>
+
+@endsection
---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>
+        @yield('title') — passwordmanager
+    </title>
+    <link rel="icon" href="{{ URL::asset('res/logo.png') }}">
+    <link rel="stylesheet" href="{{ URL::asset('css/default.css') }}">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.16.15/dist/css/uikit.min.css" />
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.16.15/dist/js/uikit.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.16.15/dist/js/uikit-icons.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    @yield('head')
+</head>
+<body>
+    @yield('content')
+
+</body>
+</html>

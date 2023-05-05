652300faf577512e76c9c884ca5600e80a691a2b
gthanksg <alamovazamjon@gmail.com>
2023-05-05 16:11:26 +0300

    breeze

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,9 +5,9 @@
 @section('content')
 
     <div class="uk-container uk-margin-large-top uk-dark">
-        <h1 class="decorated-text"><a href="#" id="style-2" data-replace="pswrdmngr"><span>pswrdmngr</span></a></h1>
+        <h1 class="">pswrdmngr</h1>
         <h3>welcome to pswrdmngr by azam alamov — simple, yet reliable password manager.<br>
-            аccess from any device, anytime, anywhere.</h3>
+            access from any device, anytime, anywhere.</h3>
     </div>
 
 @endsection
